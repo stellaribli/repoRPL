c2e59da03e1381f492da66d3479ab3e17f1f4ad5
stella.ribli <stellaribli@students.itb.ac.id>
2021-11-27 18:03:24 +0700

    checkpoint register

--- src/apistella.py
+++ src/apistella.py
@@ -1,17 +1,18 @@
+from enum import Enum
+from datetime import date, datetime, timedelta
+import enum
 import json
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from datetime import datetime, timedelta
 from typing import Optional
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.sql.sqltypes import Date
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from re import search
-from typing import List
-from fastapi import Depends, FastAPI, HTTPException, UploadFile, File
-from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI, HTTPException
 import psycopg2
 import sys
 sys.path.insert(0, './src')
@@ -20,18 +21,16 @@ import schemas
 from database import db
 from fastapi.responses import FileResponse
 import shutil
-import json
 import os
 import os.path
- 
+
 cur = db.connect()
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-class List():
-    role: str
+app = FastAPI(description ="Login Account Tuteers")
 
 class Token(BaseModel):
     access_token: str
@@ -52,18 +51,35 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
+class GenderEnum(enum.Enum):
+    Male = "Male"
+    Female = "Female"
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-with open("menu.json", "r") as read_file:
-    data = json.load(read_file)
-read_file.close()
 
 with open("user.json", "r") as read_file:
     fake_users_db = json.load(read_file)
 read_file.close()
 
-app = FastAPI(description ="Login Account Tutee")
+def hitungJumlahAkun():
+    hitungJumlahAkun = cur.execute('SELECT COUNT(*) FROM tuteers;')
+    jumlah = hitungJumlahAkun.fetchone()
+    return(jumlah[0])
+
+def makeDateFormat(year: int, month: int, date: int):
+    return(str(year)+'-'+str(month)+'-'+str(date))
+    
+def enumToStr(gender:GenderEnum):
+    if gender ==GenderEnum.Female:
+        return("Female")
+    elif gender ==GenderEnum.Male:
+        return("Male")
+    
+def updateDate(tanggal: date):
+    update_formula = 'UPDATE tuteers SET "tanggalLahir" = %s WHERE "ID_Tuteers" = 1'
+    cur.execute(update_formula, tanggal)
+    return('Gender Successfully Changed to Female!')
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -71,10 +87,14 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+#blm bener
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict) #supposed to be hash keknya
+
+print(get_user(fake_users_db,'asdf'))
+# print(get_user(cur,'asdf'))
 
 def authenticate_user(fake_db, username: str, password: str): #Login keknya
     user = get_user(fake_db, username)
@@ -127,51 +147,29 @@ async def isCurrUsername(username: str, current_user: User = Depends(get_current
     else:
         return(False)
 
-@app.get('/user', tags=["Manajemen Akun"])
-async def read_user_data(current_user: User = Depends(get_current_active_user)):
-    return fake_users_db
+@app.get('/test', tags = ['User Side'])
+async def test(current_user: User = Depends(get_current_active_user)):
+    return ("x")
 
-@app.post('/reviewer', tags=["Admin View"])
-async def post_data(name:str, current_user: User = Depends(get_current_active_user)):
-    id=1
-    if(len(data['menu'])>0):
-        id=data['menu'][len(data['menu'])-1]['id']+1
-    new_data={'id':id,'name':name}
-    data['menu'].append(dict(new_data))
-    return ("Data berhasil diinput")
-    
-@app.post('/register/admin', tags=["Manajemen Akun"])
-async def register_admin(full_name: str, email: str, tanggal_lahir:str, jenis_kelamin: str, nomor_hp: int, username:str, password:str, current_user: User = Depends(get_current_active_user)):
-    if username not in fake_users_db:
-        new_data = {"username": username, "hashed_password": get_password_hash(password),"disabled": False, "full_name": full_name, "email": email, "tanggal_lahir": tanggal_lahir, 'jenis_kelamin':jenis_kelamin, 'nomor_hp': nomor_hp}
-    fake_users_db[username] = new_data
-    read_file.close()
-    with open("user.json", "w") as write_file:
-        json.dump(fake_users_db,write_file,indent=4)
-    write_file.close()
-    return (new_data)
-
-@app.get('/data/{item_id}', tags=["Admin View"])
-async def update_data(item_id: int, name:str, current_user: User = Depends(get_current_active_user)):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            menu_item['name']=name
-        read_file.close()
-        with open("./menu.json", "w") as write_file:
-            json.dump(data,write_file,indent=4)
-        write_file.close()
-        return("Data berhasil diubah")
-    raise HTTPException(
-        status_code=404, detail=f'Item not found'
-    )
-
-@app.post('/daftarpaket', tags=["Menu Pengguna"])
-async def Baca_List_Harga():
-    return(data['menu'])
+@app.post('/registerSQL', tags = ['Manajemen Akun'])
+async def register_sql(name: str, email: str, password: str, reenterpass: str, noHP: str, year: int, month: int, date: int, gender: GenderEnum):
+    if password == reenterpass:
+        tanggal =  makeDateFormat(year,month,date)
+        genderStr = enumToStr(gender)
+        passwordhashed = get_password_hash(password)
+        query1 = 'INSERT INTO tuteers ("nama", "email", "noHP", "tanggalLahir", "gender","hashedPassword") VALUES'
+        query2 = "(%s,%s,%s,%s,%s,%s);"
+        query = query1+query2
+        values = (name, email, noHP, tanggal, genderStr, passwordhashed)   
+        item = cur.execute(query, values)
+        return('Success')
+    else:
+        return('Password Tidak Sama!')
 
 @app.post("/token", response_model=Token, tags=["Others"])
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    # yang diisi di form print(form_data.username) 
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -184,67 +182,33 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
-@app.get('/resetpassword/', tags=["Manajemen Akun"])
-async def reset_password(current_password: str, password:str,current_user: User = Depends(get_current_active_user)):
-    if verify_password(current_password,(fake_users_db[current_user.username])['hashed_password']):
-        (fake_users_db[current_user.username])['hashed_password'] = get_password_hash(password)
-        with open("user.json", "w") as write_file:
-            json.dump(fake_users_db,write_file,indent=4)
-        write_file.close()
-        return('Password berhasil diubah!')
-    else:
-        return('Password salah!')
-
-@app.get("/tuteers")
-async def gettuteers():
-    item = cur.execute('SELECT * FROM tuteers')
-    result = item.fetchall()
-    return result
-@app.get('/getjson')
-async def getjson():
-    return(fake_users_db)
-
-@app.get("/reviewer")
-async def getreviewer():
-    item = cur.execute('SELECT * FROM reviewer')
-    result = item.fetchall()
-    return result[0]
-    
-@app.get('/resetpasswordsql/', tags=["Manajemen Akun"])
-async def reset_passwordsql(input: str):
-    # if verify_password(current_password,(gettuteers()[current_user.username])['hashed_password']):
-    #     (gettuteers()[current_user.username])['hashed_password'] = get_password_hash(password)
-    #     with open("user.json", "w") as write_file:
-    #         json.dump(gettuteers(),write_file,indent=4)
-    #     write_file.close()
-    #     return('Password berhasil diubah!')
-    # else:
-    #     return('Password salah!')
-    update_formula = 'UPDATE tuteers SET "hashedPassword" = %s WHERE "ID_Tuteers" = 1'
+@app.get('/resetPasswordSQL/', tags=["Manajemen Akun"])
+async def reset_password_sql(input: str):
+    update_formula = 'UPDATE "tuteers" SET "hashedPassword" = %s WHERE "ID_Tuteers" = 1'
     values = (get_password_hash(input))
     item = cur.execute(update_formula, values)
     return ("Query Update Success")
 
+@app.get("/tuteers")
+async def gettuteers():
+    item = cur.execute('SELECT email FROM tuteers')
+    result = item.fetchall()
+    return result
 
-@app.get('/gantinohp')
-async def gantinohp(nohp: str):
-    update_formula = 'UPDATE tuteers SET "noHP" = %s WHERE "ID_Tuteers" = 1'
-    values = (nohp)
-    item = cur.execute(update_formula, values)
-    return ("Query Update Success")
-
-@app.get('/apakahpasswordsama')
-async def samaga():
-    select_formula = 'SELECT "hashedPassword" FROM tuteers WHERE "ID_Tuteers" = 1;'
-    item = cur.execute(select_formula)   
-    result = item.fetchone()
-    if verify_password("asdf", result[0]):
-         return ("Biza")
-    else:
-        return ("h")
-    # return result[0]
-
-@app.get('/hashpass')
-async def hashingpass(pas: str):
-    return(get_password_hash(pas))
-
+# @app.get('/apakahpasswordsama')
+# async def samaga(a: str):
+#     select_formula = 'SELECT "hashedPassword" FROM tuteers WHERE "ID_Tuteers" = 1;'
+#     item = cur.execute(select_formula)   
+#     result = item.fetchone()
+#     if verify_password(a, result[0]):
+#          return ("Biza")
+#     else:
+#         return ("h")
+# @app.post('/reviewer', tags=["Admin View"])
+# async def post_data(name:str, current_user: User = Depends(get_current_active_user)):
+#     id=1
+#     if(len(data['menu'])>0):
+#         id=data['menu'][len(data['menu'])-1]['id']+1
+#     new_data={'id':id,'name':name}
+#     data['menu'].append(dict(new_data))
+#     return ("Data berhasil diinput")
